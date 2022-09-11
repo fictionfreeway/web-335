ae2c2fac66bd493ad7d712dc4d624d42b076a20c
beastmodewatt <fictionfreeway@gmail.com>
2022-09-11 17:46:18 -0400

    week 5 python

---
+++ week-5/watlington_hobbies.py
@@ -0,0 +1,23 @@
+""" 
+Title: watlington-hobbies.js
+Author: William Watlington
+Date: 11 September 2022
+Description: Python Conditionals/Lists/Loops assignment for WEB 335
+ """
+
+hobbies = ["reading", "gaming", "magic", "racing", "pc building"]
+
+for hobby in hobbies:
+    print(hobby)
+
+days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+
+for day in days:
+    if day == "sunday" or day == "saturday":
+        print("Today you are off. It is: " + day)
+    else:
+        print("Today is a work day. It is: " + day)
+
+input("test")
+
+        
