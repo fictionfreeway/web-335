86f4322f3d95fc60b4f201904c0ad51e8bb5cab3
William Watlington <fictionfreeway@gmail.com>
2022-09-13 16:41:53 -0400

    week 6 python complete

---
+++ week-6/watlington_usersp1.py
@@ -0,0 +1,26 @@
+""" 
+Title: watlington-userssp1.py
+Author: William Watlington
+Date: 13 September 2022
+Description: Basic python connection to MongoDB and basic queries
+"""
+
+# import MongoDB client
+from pymongo import MongoClient
+
+
+# string from MongoDB for to connect to database
+client = MongoClient("mongodb+srv://web335_user:s3cret@cluster0.ug54bka.mongodb.net/web33DBretryWrites=true&w=majority")
+
+# variable for accessing the web335 database
+db = client['web33DB']
+
+# print all documents in users collection
+for user in db.users.find( {} ):  
+    print(user)
+
+# print user document with employeeId equal to 1011
+print(db.users.find_one( { "employeeId": "1011" } ))
+
+# print user document with lastName Mozart
+print(db.users.find_one( { "lastName": "Mozart" } ))
